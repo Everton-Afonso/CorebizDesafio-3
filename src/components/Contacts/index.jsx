ae369a3e82838fee3c0cdb73c41f8d3d9d0fb2c7
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-18 09:40:52 -0300

    alteração dos icons do componente banner

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { FaWhatsapp, FaPhoneAlt, FaRegEnvelope } from "react-icons/fa";
 
 import "./styles.css";
 
 function Navigation() {
   const whatsapp = 5535998511667,
-    phone = 5535998511667,
-    email = "everton@gmail.com";
+    linkedin = "https://www.linkedin.com/in/everton-otavio-b5b035191/",
+    github = "https://github.com/Everton-Afonso";
 
   return (
     <section className="container-contacts">
@@ -15,18 +14,26 @@ function Navigation() {
           className="contacts-link"
           href={`https://wa.me/send?phone=${whatsapp}&text=Texto%20Olá em que posso te ajudar`}
         >
-          <FaWhatsapp className="contacts-icons" />
+          <img
+            src="icons/whatsapp.png"
+            alt="Whatsaap"
+            className="contacts-icons"
+          />
           <span>Whatsapp</span>
         </a>
 
-        <a className="contacts-link" href={`tel:${phone}`}>
-          <FaPhoneAlt className="contacts-icons" />
-          <span>Telefone</span>
+        <a className="contacts-link" href={linkedin}>
+          <img
+            src="icons/linkedin.png"
+            alt="Linkedin"
+            className="contacts-icons"
+          />
+          <span>Linkedin</span>
         </a>
 
-        <a className="contacts-link" href={`mailto:${email}`}>
-          <FaRegEnvelope className="contacts-icons" />
-          <span>E-mail</span>
+        <a className="contacts-link" href={github}>
+          <img src="icons/github.png" alt="GitHub" className="contacts-icons" />
+          <span>GitHub</span>
         </a>
       </div>
     </section>
